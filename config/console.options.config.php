381aed72972c36b0c6ed24a83c3e554246aa9ae1
StefanieD <stefaniedrost@gmx.de>
2019-05-17 11:25:20 +0200

    configure days per crawler of organization

--- config/console.options.config.php
+++ config/console.options.config.php
@@ -3,13 +3,27 @@
 /*
  * Configuration for command delete expired jobs.
  *
+ * Define how long a job should be inactive before it will be deleted from database.
+ *
  */
 
 $options = [
 
     'crawler' => [
-        'days' => 30
-
+        'organizations' => [
+            'Adecco' => [
+                'days' => 30
+            ],
+            'McDonald\'s Suisse Management & Services Sàrl' => [
+                'days' => 30
+            ],
+            'Gilde Restaurants' => [
+                'days' => 30
+            ],
+            'zfv Die Gastronomiegruppe'=> [
+                'days' => 30
+            ]
+        ]
     ],
     'paid' => [
         'days' => 365
--- src/Controller/Console/DeleteJobsController.php
+++ src/Controller/Console/DeleteJobsController.php
@@ -50,28 +50,49 @@ class DeleteJobsController extends AbstractActionController
     {
         /* @var \Jobs\Repository\Job $jobsRepo */
         $jobsRepo = $this->repositories->get('Jobs/Job');
-        //TODO: get days from options
-        $days = 30;
+        $orgRepo = $this->repositories->get('Organizations/Organization');
 
+        echo "Clear paid jobs ...\n";
+        $days = $this->options->getPaid()['days'];
         $date = new \DateTime('today');
         $date->sub(new \DateInterval('P' . $days . 'D'));
 
-        $query = [
-            '$and' => [
-                ['status.name' => StatusInterface::EXPIRED],
-                ['$or' => [
-                    ['datePublishStart.date' => ['$lt' => $date]],
-                    ['datePublishEnd.date' => ['$lt' => new \DateTime('today midnight')]],
-                ]],
-                ['user' => ['$exists' => false]], // fetch only Einzelinserate
-            ]
-        ];
-
+        $orgKeys = array_keys($this->options->getCrawler()['organizations']);
+        $query = $this->getQueryForPaidJobs($date, $orgKeys);
         $jobs = $jobsRepo->findBy($query);
+        $this->clearJobs($jobsRepo, $jobs);
+
+
+        echo "Clear crawler jobs ...\n";
+        foreach($this->options->getCrawler()['organizations'] as $organization => $values) {
+            $days = $values['days'];
+            $date = new \DateTime('today');
+            $date->sub(new \DateInterval('P' . $days . 'D'));
+
+            $org = $orgRepo->findOneBy(['_organizationName' => $organization]);
+            if (!$org) {
+                continue;
+            }
+
+            $query = $this->getQueryForCrawlerJobs($date, $org->getId());
+            $jobs = $jobsRepo->findBy($query);
+
+            $this->clearJobs($jobsRepo, $jobs);
+        }
+    }
+
+    /**
+     * @param \Jobs\Repository\Job $jobsRepo
+     * @param $jobs
+     * @return string
+     */
+    private function clearJobs($jobsRepo, $jobs)
+    {
         $count = count($jobs);
 
         if (0 === $count) {
-            return 'No jobs found.';
+            echo "No jobs found.\n";
+            return PHP_EOL;
         }
 
         echo "$count jobs found, which will be deleted ...\n";
@@ -97,6 +118,14 @@ class DeleteJobsController extends AbstractActionController
 
         $i = 0;
 
+        // remove from SOLR
+        /* @var \Jobs\Entity\Job $job */
+        foreach ($jobs as $job) {
+            $job->delete();
+        }
+        $this->repositories->flush();
+
+        // remove from MongoDB
         /* @var \Jobs\Entity\Job $job */
         foreach ($jobs as $job) {
             $progress->update($i++, 'Job ' . $i . ' / ' . $count);
@@ -108,11 +137,42 @@ class DeleteJobsController extends AbstractActionController
                 $this->repositories->flush();
             }
         }
+
         $progress->update($i, 'Remove from database...');
         $this->repositories->flush();
         $progress->update($i, 'Done');
         $progress->finish();
 
         return PHP_EOL;
+    }
+
+    private function getQueryForPaidJobs($date, $orgKeys)
+    {
+        return [
+            '$and' => [
+                ['status.name' => StatusInterface::EXPIRED],
+                ['$or' => [
+                    ['datePublishStart.date' => ['$lt' => $date]],
+                    ['datePublishEnd.date' => ['$lt' => new \DateTime('today midnight')]],
+                ]],
+                ['user' => ['$exists' => true]],
+                ['organization' => ['$nin' => $orgKeys]],
+            ]
+        ];
+    }
+
+    private function getQueryForCrawlerJobs($date, $orgId)
+    {
+        return [
+            '$and' => [
+                ['status.name' => StatusInterface::EXPIRED],
+                ['$or' => [
+                    ['datePublishStart.date' => ['$lt' => $date]],
+                    ['datePublishEnd.date' => ['$lt' => new \DateTime('today midnight')]],
+                ]],
+                ['user' => ['$exists' => true]],
+                ['organization' => ['$eq' => $orgId]],
+            ]
+        ];
     }
 }
