e70b0525844beba69940f0947d329ff0ac9c65af
StefanieD <stefaniedrost@gmx.de>
2019-05-17 08:40:34 +0200

    remove header Content-Length from FileController indexAction

--- config/module.config.php
+++ config/module.config.php
@@ -91,6 +91,7 @@ return [
             Controller\SuggestJobs::class => Factory\Controller\SuggestJobFactory::class,
             Controller\OrdersController::class => Factory\Controller\OrdersControllerFactory::class,
             'Gastro24/Jobs/Console' => [Controller\Console\JobsConsoleController::class,'factory'],
+            'Core/File'    => Factory\Controller\FileControllerFactory::class,
         ],
     ],
 
