faa2ab7d451fab8cf6dbe30742937e64d4145f1b
StefanieD <stefaniedrost@gmx.de>
2019-09-23 11:40:27 +0200

    optimize synonym suggests

--- src/Controller/SuggestJobs.php
+++ src/Controller/SuggestJobs.php
@@ -12,6 +12,8 @@ use Zend\Mvc\Controller\AbstractActionController;
  */
 class SuggestJobs extends AbstractActionController
 {
+    const SUGGEST_LIMIT = 5;
+
     /**
      * @var \Solr\Options\ModuleOptions
      */
@@ -49,21 +51,70 @@ class SuggestJobs extends AbstractActionController
         $collection = $data->toArray();
 
         foreach ($collection as $item) {
-            if (strpos($item->getMainWord(), $searchTerm) !== false) {
+            if (stripos($item->getMainWord(), $searchTerm) !== false) {
                 foreach ($item->getWordsWithSameMeaning() as $word => $wordString) {
-                    $results[$word] = [
-                        'value' => $wordString,
-                        'id' => $wordString,
-                        'label' => $wordString
-                    ];
+                    $results[] = $wordString;
                 }
-
             }
         }
+        $results = array_unique($results);
 
-        $results = array_slice($results, 0, 10);
-        $response->setContent(json_encode($results));
+        //@see https://stackoverflow.com/a/37974879/8465974
+        usort($results,function($a,$b) use ($searchTerm) {
+            // prioritize exact matches first
+            if ($a == $searchTerm) return -1;
+            if ($b == $searchTerm) return 1;
+
+            // prioritize terms containing the keyword next
+            $x = stripos($a, $searchTerm);
+            $y = stripos($b, $searchTerm);
+            if ($x !== false && $y === false) return -1;
+            if ($y !== false && $x === false) return 1;
+            if ($x !== false && $y !== false) {  // both terms contain the keyword, so...
+
+                if ($x != $y) {  // prioritize matches closer to the beginning of the term
+                    return $x > $y ? 1 : -1;
+                }
+
+                // both terms contain the keyword at the same position, so...
+                $al = strlen($a);
+                $bl = strlen($b);
+                if ($al != $bl) { // prioritize terms with fewer characters other than the keyword
+                    return $al > $bl ? 1 : -1;
+                }
+                // both terms contain the same number of additional characters
+                return 0;
+                // or sort alphabetically with strcmp($a, $b);
+                // or do additional checks...
+            }
+            // neither terms contain the keyword
+
+            // check the character similarity...
+            $ac = levenshtein($searchTerm, $a);
+            $bc = levenshtein($searchTerm, $b);
+            if ($ac != $bc) {
+                return $ac > $bc ? 1 : -1;
+            }
+
+            return 0;
+            // or sort alphabetically with strcmp($a, $b);
+            // or do additional checks, similar_text, etc.
+        });
+
+        $results = array_slice($results, 0, self::SUGGEST_LIMIT);
+        $finalResults = [];
+        foreach ($results as $word) {
+            $finalResults[$word] = [
+                'value' => $word,
+                'id' => $word,
+                'label' => $word
+            ];
+        }
+
+        $response->setContent(json_encode($finalResults));
 
         return $response;
     }
+
+
 }
