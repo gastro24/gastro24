7d2bf6c34db789ed9fa00b9902426efabe23f8e3
Marc Bösch <marc.boesch@gmx.ch>
2020-02-04 18:45:19 +0000

    Update landingpages.config.php

--- config/landingpages.config.php
+++ config/landingpages.config.php
@@ -37,7 +37,7 @@ $options = [
   
       'active-gastro-eng-gmbh' => [
           'id' => 1540,
-		  'query' => [ 'q' => 'Active Gastro Eng GmbH'],
+		  'query' => [ 'q' => 'Active Gastro'],
 		  'tab' => '',
 		  'panel' => '',
           'text' => 'Jobs Active Gastro Eng GmbH'
