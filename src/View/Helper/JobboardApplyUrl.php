79ebb1eaa37ce1abecd33bc2dbf248648234b7ce
Marc Bösch <marc.boesch@gmx.ch>
2020-08-23 11:41:25 +0000

    Update JobboardApplyUrl.php

--- src/View/Helper/JobboardApplyUrl.php
+++ src/View/Helper/JobboardApplyUrl.php
@@ -50,12 +50,14 @@ class JobboardApplyUrl extends AbstractHelper
             $url  = $this->urlHelper->__invoke($route, $params);
             $class = 'internal-apply-link';
             $text = 'Jetzt bewerben';
+            $pdflink = $job->getLink();
             //$pdflink = '.pdf' == substr($job->getLink(), -4) ?$job->getLink() : null;
 
         } else {
             $url = $ats->getUri();
             $class = 'external-apply-link';
             $text = 'Jetzt bewerben';
+            $pdflink = $job->getLink();
             //$pdflink = '.pdf' == substr($job->getLink(), -4) ?$job->getLink() : null;
         }
 
