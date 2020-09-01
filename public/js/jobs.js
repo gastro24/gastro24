2c1e78fe4a93fb159276be1a303c0bbca015c57c
Marc Bösch <marc.boesch@gmx.ch>
2020-09-01 15:10:15 +0000

    Update jobs.js

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,36 +1,15 @@
-/**
- * YAWIK
- *
- * License: MIT
- * (c) 2013 - 2017 CROSS Solution <http://cross-solution.de>
- */
-
-/**
- *
- * Author: Mathias Gelhausen <gelhausen@cross-solution.de>
- */
 ;(function ($, window) {
-
-    function onPagiantorLoaded()
-    {
-        $('.featured-image-box').matchHeight({
-            byRow: true,
-            property: 'height',
-            target: null,
-            remove: false
-        });
-    }
 
     function onApplyLinkClicked(event)
     {
         var $link = $(event.currentTarget);
         var isExternal = $link.hasClass('external-apply-link');
         var message = (isExternal ? '' : '<p>Um Ihre Bewerbung zu senden, nutzen Sie bitte die Möglichkeit im Inserat</p>')
-                    + '<p><strong>Bitte beziehen Sie sich bei Ihrer Bewerbung auf Gastrojob24.ch. Vielen Dank.</strong></p>';
+                    + '<p><strong>Bitte beziehen Sie sich bei Ihrer Bewerbung auf Gastrojob24. Vielen Dank.</strong></p>';
         var uri = $link.attr('href');
 
         BootstrapDialog.show({
-            title: $('<h5 class="modal-title">Jetzt auf die Stelle bewerben</h5>'),
+            title: $('<h5 class="modal-title">Jetzt bewerben</h5>'),
             type: BootstrapDialog.TYPE_DEFAULT,
             message: $(message),
             closable: true,
@@ -102,11 +81,8 @@
         var $container = $('#jobs-list-container');
 
         if ($container.length) {
-            $('#jobs-list-container').on('yk-paginator-container:loaded.jobboard', onPagiantorLoaded)
-                .on('click.jobboard', '.internal-apply-link', onInternalApplyLinkClicked)
+            $('#jobs-list-container').on('click.jobboard', '.internal-apply-link', onInternalApplyLinkClicked)
                 .on('click.jobboard', '.external-apply-link, .no-apply-link', onApplyLinkClicked);
-
-            onPagiantorLoaded();
 
         } else {
             $('a.internal-apply-link').click(onInternalApplyLinkClicked);
@@ -162,4 +138,3 @@
     });
 
 })(jQuery, window); 
- 
