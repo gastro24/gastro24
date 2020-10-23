640d4c303c4a504705843917c4d150f5e54e7ec3
Marc Bösch <marc.boesch@gmx.ch>
2020-10-23 18:38:30 +0000

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -680,7 +680,7 @@ return [
         Options\TopJobsOptions::class => [[
             'organizations' => [
                 /* organization name => boost param */
-                'Berghotel Sterna' => 55,
+                'Berghotel Sterna' => 10,
                 'GST gastro.jobs AG' => 5,
             ],
             'jobs' => [
