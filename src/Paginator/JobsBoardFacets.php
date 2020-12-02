dd4c2992b2020f47b2982c22e433c71240d3890e
Marc Bösch <marc.boesch@gmx.ch>
2020-12-02 17:10:26 +0000

    Update JobsBoardFacets.php

--- src/Paginator/JobsBoardFacets.php
+++ src/Paginator/JobsBoardFacets.php
@@ -77,7 +77,7 @@ class JobsBoardFacets extends Facets
                 $activeValues = [];
 
                 foreach ($values as $value => $count) {
-                    if (isset($this->params[$name][$value]) && $count >=1) {
+                    if (isset($this->params[$name][$value])) {
                         $activeValues[] = $value;
                     }
                 }
