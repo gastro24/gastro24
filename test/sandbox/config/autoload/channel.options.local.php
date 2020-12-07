961cc3037e632474a6a0ee0ebde58a90153e1cac
StefanieD <stefaniedrost@gmx.de>
2020-12-07 08:45:42 +0100

    #596 - add some additional values to main channel options file

--- test/sandbox/config/autoload/channel.options.local.php
+++ test/sandbox/config/autoload/channel.options.local.php
@@ -14,6 +14,8 @@ $channel['social-media'] = array(
 
     'label' => 'Job-Boost 1',
     'prices' => [ 'base' => 150, 'list' => 150, 'min'  => 150, ],
+    'currency' => 'SFR',
+    'tax' => 0,
     'headline' => /*@translate*/ 'Social Media Werbung',
     'description' => /*@translate*/ 'Publikation auf Facebook & Instagram (inkl. CHF 50.- Ads-Budget) während 5 Tagen.',
     'route' => 'lang/content',
@@ -27,6 +29,8 @@ $channel['top-listing-home'] = array(
 
     'label' => 'Job-Boost 2',
     'prices' => [ 'base' => 95, 'list' => 95, 'min'  => 95, ],
+    'currency' => 'SFR',
+    'tax' => 0,
     'headline' => /*@translate*/ 'Top-Listing Homepage',
     'description' => /*@translate*/ 'Premium-Platzierung oben auf der Startseite während 30 Tagen.',
     'route' => 'lang/content',
@@ -40,6 +44,8 @@ $channel['top-listing-search'] = array(
 
     'label' => 'Job-Boost 3',
     'prices' => [ 'base' => 55, 'list' => 55, 'min'  => 55, ],
+    'currency' => 'SFR',
+    'tax' => 0,
     'headline' => /*@translate*/ ' Top-Listing Suche',
     'description' => /*@translate*/ 'Kommt Ihre Stellenanzeige in einer Suche vor, wird sie automatisch während 30 Tagen oberhalb der klassischen Stelleninserate angezeigt und hervorgehoben.',
     'route' => 'lang/content',
