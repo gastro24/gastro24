84d6d4c471c71e8f9c70a4e9df7b0c28c8e72fd2
Marc Bösch <marc.boesch@gmx.ch>
2021-01-18 19:58:21 +0000

    Update JobboardApplyUrl.php

--- src/View/Helper/JobboardApplyUrl.php
+++ src/View/Helper/JobboardApplyUrl.php
@@ -65,6 +65,6 @@ class JobboardApplyUrl extends AbstractHelper
          //   $pdflink = ' <a href="' . $pdflink . '" class="btn btn-primary">PDF downloaden</a>';
         }
 
-        return $url?sprintf('<a rel="nofollow" href="%s" class="btn btn-primary %s">%s</a>%s', $url, $class, $text, $pdflink):'';
+        return $url?sprintf('<a rel="nofollow" href="%s" class="btn btn-primary %s">%s &nbsp;<svg fill="#ffffff" viewBox="0 0 20 20" width="12px" height="12px"><path d="M5.408.153a.588.588 0 00-.098.755l.059.076L13.566 10 5.37 19.016a.588.588 0 00-.025.761l.065.07c.216.197.54.202.761.026l.07-.066 8.27-9.096c.337-.372.363-.925.077-1.324l-.078-.097L6.24.193a.588.588 0 00-.832-.04z" fill-rule="evenodd"></path></svg></a>%s', $url, $class, $text, $pdflink):'';
     }
 }
