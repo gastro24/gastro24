81c1f2f3aad67aa38469dbbe83b24013fda82891
Marc Bösch <marc.boesch@gmx.ch>
2021-01-30 16:00:11 +0000

    Update de_DE-override.php

--- language/de_DE-override.php
+++ language/de_DE-override.php
@@ -30,6 +30,7 @@ return [
     '%1$s - %2$s of %3$s' => '%1$s - %2$s von %3$s',
     'Job, Keyword or Location' => 'Beruf, Begriff oder Arbeitsort',
     'Job or Keyword' => 'Beruf oder Begriff',
+    'search' => 'Jobs finden',
     'Location or Zip' => 'Ort oder PLZ',
     'Back to search' => 'Zurück',
     'Previous Job' => 'Vorheriger Job',
