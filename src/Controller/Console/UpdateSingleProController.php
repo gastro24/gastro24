5c91a55789a3dbd959776389c199365cbfa6426a
StefanieD <stefaniedrost@gmx.de>
2021-03-23 09:16:42 +0100

    #631 - solr update after publish date was updated

--- src/Controller/Console/UpdateSingleProController.php
+++ src/Controller/Console/UpdateSingleProController.php
@@ -8,6 +8,7 @@ use Jobs\Entity\StatusInterface;
 use Laminas\Log\LoggerInterface;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Orders\Entity\Order;
+use Solr\Filter\EntityToDocument\JobEntityToSolrDocument as EntityToDocumentFilter;
 
 /**
  * Console Controller which updated publishedDate of Single Pro Jobs
@@ -22,22 +23,42 @@ class UpdateSingleProController extends AbstractActionController
     private $repositories;
 
     /**
+     * @var SolrClient
+     */
+    private $solrClient;
+
+    /**
+     * @var EntityToDocumentFilter
+     */
+    protected $entityToDocumentFilter;
+
+    /**
      * @var LoggerInterface
      */
     private $logger;
 
     public function __construct(
+        $solrClient,
         RepositoryService $repositories,
+        EntityToDocumentFilter $entityToDocumentFilter,
         $logger
     ) {
+        $this->solrClient = $solrClient;
         $this->repositories = $repositories;
+        $this->entityToDocumentFilter = $entityToDocumentFilter;
         $this->logger = $logger;
     }
 
     public static function factory(ContainerInterface $container)
     {
+        $manager = $container->get('Solr/Manager');
+        $options = $container->get('Solr/Options/Module');
+        $solrClient = $manager->getClient($manager->getOptions()->getJobsPath());
+
         return new self(
+            $solrClient,
             $container->get('repositories'),
+            new EntityToDocumentFilter($options),
             $container->get('Core/Log')
         );
     }
@@ -86,6 +107,13 @@ class UpdateSingleProController extends AbstractActionController
 
                 $job->setDatePublishStart();
                 $this->repositories->store($job);
+
+                $document = $this->entityToDocumentFilter->filter($job);
+                $this->solrClient->addDocument($document);
+
+                // commit to index & optimize it
+                $this->solrClient->commit(true, false);
+                $this->solrClient->optimize(1, true, false);
             }
         }
         $this->repositories->flush();
